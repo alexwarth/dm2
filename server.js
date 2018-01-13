ed71d5ff59497ac59dc672796369fa416d3b2606
Jonathan Dahan <hi@jonathan.is>
2018-01-12 20:03:02 -0500

    We now can learn to not be afraid of mice, and have eyes

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const Koa = require('koa')
-const static = require('koa-static')
+const koaStatic = require('koa-static')
 const route = require('koa-route')
 const websockify = require('koa-websocket')
 
 const app = websockify(new Koa())
 
-app.use(static('./'))
+app.use(koaStatic('./'))
 
 app.ws.use(route.all('/', (ctx) => {
-  ctx.websocket.send(['Hello World'])
+  ctx.websocket.on('connect', info => {
+    console.log(info)
+  })
   ctx.websocket.on('message', message => {
-    console.log(message)
+    ctx.websocket.send(message)
   })
 }))
 
-app.listen(3000, info => {
-  console.log(info)
-})
+app.listen(3000)
