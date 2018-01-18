9e68f63b77a1f8110822b0dedd389645d7773589
Alessandro Warth <alexwarth@gmail.com>
2018-01-18 12:15:15 -0500

    switch to new RoomDB library (which lives in a separate repo)

--- db.js
+++ db.js
@@ -1,14 +1,15 @@
 'use strict'
 
-const db = new Database(
-  ['C1', 'is a', 'circle', 'at', '(', 300, ',', 300, ')'],
-  ['C1', '\'s', 'color', 'is', 'cornflowerblue'],
-  ['C1', '\'s', 'radius', 'is', 50],
-  ['R1', 'is a', 'rectangle', 'at', '(', 450, ',', 150, ')'],
-  ['R1', '\'s', 'width', 'is', 50],
-  ['R1', '\'s', 'height', 'is', 100],
-  ['R1', '\'s', 'color', 'is', 'salmon']
-)
+const db = new RoomDB()
+const client = db.connect()
+client.assert('#C1 is a circle at (300, 300)')
+client.assert('#C1\'s color is "cornflowerblue"')
+client.assert('#C1\'s radius is 50')
+client.assert('#C1 is a circle at (300, 300)')
+client.assert('#R1 is a rectangle at (450, 150)')
+client.assert('#R1\'s width is 50')
+client.assert('#R1\'s height is 100')
+client.assert('#R1\'s color is "salmon"')
 
 class Orchestrator extends Rectangle {
   constructor (...processes) {
@@ -26,6 +27,7 @@ class Orchestrator extends Rectangle {
         : this.processes[this.processes.length - 1]
     const process = new Rectangle(20, lastObj.y + 40, 30, 30, color)
     process.name = name
+    process.client = db.connect()
     process.drawOn = function (ctxt, options) {
       ctxt.font = '12pt optima'
       this.width = ctxt.measureText(this.name).width + 8
@@ -47,7 +49,7 @@ class Orchestrator extends Rectangle {
     }
     process.step = t => {
       if (!process.disabled) {
-        fn(t)
+        fn(process.client, t)
       }
     }
     process.disabled = !!optDisabled
@@ -58,7 +60,7 @@ class Orchestrator extends Rectangle {
 const orchestrator = new Orchestrator()
 objects.push(orchestrator)
 
-function illuminate (fn) {
+function illuminate(fn) {
   const r = new Rectangle(0, 0, 0, 0, 'black')
   r.drawOn = () => {}
   r.drawOverOn = fn
@@ -68,53 +70,66 @@ function illuminate (fn) {
 orchestrator.addProcess(
   'circle illuminator',
   'steelblue',
-  () => {
-    db.query(
-      [
-        ['$c', 'is a', 'circle', 'at', '(', '$x', ',', '$y', ')'],
-        ['$c', '\'s', 'radius', 'is', '$r'],
-        ['$c', '\'s', 'color', 'is', '$color']
-      ],
-      ({$x, $y, $r, $color}, facts, assert, retract) => {
-        const c = new Circle($x, $y, $r, $color)
-        c.fact = facts[0]
-        objects.push(c)
-      }
-    )
+  client => {
+    client.select(
+      '$obj is a circle at ($x, $y)',
+      '$obj\'s radius is $r',
+      '$obj\'s color is $color'
+    ).do(({obj, x, y, r, color}) => {
+      const c = new Circle(x, y, r, color)
+      c.id = obj
+      objects.push(c)
+    })
   })
 
 orchestrator.addProcess(
   'rectangle illuminator',
   'salmon',
-  () => {
-    db.query(
-      [
-        ['$r', 'is a', 'rectangle', 'at', '(', '$x', ',', '$y', ')'],
-        ['$r', '\'s', 'width', 'is', '$w'],
-        ['$r', '\'s', 'height', 'is', '$h'],
-        ['$r', '\'s', 'color', 'is', '$color']
-      ],
-      ({$x, $y, $w, $h, $color}, facts, assert, retract) => {
-        const r = new Rectangle($x, $y, $w, $h, $color)
-        r.fact = facts[0]
-        objects.push(r)
-      }
-    )
+  client => {
+    client.select(
+      '$obj is a rectangle at ($x, $y)',
+      '$obj\'s width is $w',
+      '$obj\'s height is $h',
+      '$obj\'s color is $color'
+    ).do(({obj, x, y, w, h, color}) => {
+      const r = new Rectangle(x, y, w, h, color);
+      r.id = obj
+      objects.push(r);
+    })
   })
 
 orchestrator.addProcess(
   'object labeler',
   'maroon',
-  () => {
-    db.query(
-      [['$c', 'is a', '$t', 'at', '(', '$x', ',', '$y', ')']],
-      ({$c, $x, $y}, facts, assert, retract) => {
-        illuminate(ctxt => {
-          ctxt.fillStyle = 'lightyellow'
-          ctxt.fillText($c, $x - ctxt.measureText($c).width / 2, $y + 6)
-        })
-      }
-    )
+  client => {
+    client.select(
+      '$id is a $t at ($x, $y)'
+    ).do(({id, x, y}) => {
+      illuminate(ctxt => {
+        const label = id.toString();
+        ctxt.fillStyle = 'lightyellow'
+        ctxt.fillText(label, x - ctxt.measureText(label).width / 2, y + 6)
+      })
+    })
+  })
+
+orchestrator.addProcess(
+  'halos',
+  '#abc',
+  client => {
+    client.select(
+      '$obj is a circle at ($x, $y)',
+      '$obj\'s radius is $r'
+    ).do(({x, y, r}) => {
+      illuminate(ctxt => {
+        ctxt.beginPath()
+        ctxt.strokeStyle = 'yellow'
+        ctxt.lineWidth = 10
+        ctxt.arc(x, y, r + 20, 0, Math.PI * 2)
+        ctxt.stroke()
+        ctxt.linewidth = 1
+      })
+    })
   },
   true)
 
@@ -122,129 +137,116 @@ let globalVy = 0
 orchestrator.addProcess(
   'gravity',
   'midnightblue',
-  () => {
+  client => {
     globalVy += 1
-    db.query(
-      [['$obj', 'is a', '$t', 'at', '(', '$x', ',', '$y', ')']],
-      ({$obj, $x, $y}, facts, assert, retract) => {
-        retract(facts[0])
-        const newFact = facts[0].slice()
-        newFact[7] += globalVy
-        assert(newFact).withoutEvidence()
-      })
+    client.select(
+      '$obj is a $t at ($x, $y)'
+    ).do(({obj, t, x, y}) => {
+      client.retract('_ is a $t at ($x, $y)', obj)
+      client.assert(`${obj} is a ${t} at (${x}, ${y + globalVy})`)
+    })
   },
   true)
 
-function dist (ax, ay, bx, by) {
+function dist(ax, ay, bx, by) {
   return Math.pow(Math.pow(ax - bx, 2) + Math.pow(ay - by, 2), 0.5)
 }
 
 orchestrator.addProcess(
-  'draw sightlines',
-  'red',
-  () => {
-    db.query(
-      [['$obj', 'is a', '$t', 'at', '(', '$x', ',', '$y', ')'],
-       ['$obj2', 'is a', '$t2', 'at', '(', '$x2', ',', '$y2', ')'],
-       ['$obj', 'can see', '$obj2']
-      ],
-      ({$obj, $obj2, $x, $y, $x2, $y2}, facts, assert, retract) => {
-        illuminate(ctxt => {
-          ctxt.beginPath()
-          ctxt.strokeStyle = 'yellow'
-          ctxt.lineWidth = 10
-          ctxt.moveTo($x, $y)
-          ctxt.lineTo($x2, $y2)
-          ctxt.stroke()
-          ctxt.linewidth = 1
-        })
+  'seeing',
+  'lightgreen',
+  client => {
+    client.retractEverythingAssertedByMe();
+    client.select(
+      '$obj1 is a $t1 at ($x1, $y1)',
+      '$obj2 is a $t2 at ($x2, $y2)'
+    ).do(({obj1, x1, y1, obj2, x2, y2}) => {
+      const d = dist(x1, y1, x2, y2)
+      if (d < 300) {
+        client.assert('_ can see _', obj1, obj2)
+        client.assert('_ can see _', obj2, obj1)
       }
-    )
+    })
   },
   true)
 
 orchestrator.addProcess(
-  'seeing',
-  'lightgreen',
-  () => {
-    db.query([
-       ['$obj1', 'is a', '$t1', 'at', '(', '$x1', ',', '$y1', ')'],
-       ['$obj2', 'is a', '$t2', 'at', '(', '$x2', ',', '$y2', ')']
-    ],
-      ({$obj1, $obj2, $x1, $y1, $x2, $y2}, facts, assert, retract) => {
-        const d = dist($x1, $y1, $x2, $y2)
-        if (d < 300) {
-          assert([$obj1, 'can see', $obj2])
-          assert([$obj2, 'can see', $obj1])
-        } else {
-          retract([$obj1, 'can see', $obj2])
-          retract([$obj2, 'can see', $obj1])
-        }
-      }
-    )
+  'draw sightlines',
+  'red',
+  client => {
+    client.select(
+      '$obj1 is a $t1 at ($x1, $y1)',
+      '$obj2 is a $t2 at ($x2, $y2)',
+      '$obj1 can see $obj2'
+    ).do(({x1, y1, x2, y2}) => {
+      illuminate(ctxt => {
+        ctxt.beginPath()
+        ctxt.strokeStyle = 'yellow'
+        ctxt.lineWidth = 10
+        ctxt.moveTo(x1, y1)
+        ctxt.lineTo(x2, y2)
+        ctxt.stroke()
+        ctxt.linewidth = 1
+      })
+    })
   },
   true)
 
 orchestrator.addProcess(
   'is near a mouse',
   'orange',
-  () => {
-    db.query([
-      ['$obj', 'is a', '$t', 'at', '(', '$x', ',', '$y', ')'],
-      ['mouse', 'is', 'at', '(', '$mx', ',', '$my', ')']
-    ],
-    ({$obj, $mx, $my, $x, $y}, facts, assert, retract) => {
-      const d = dist($mx, $my, $x, $y)
+  client => {
+    client.retractEverythingAssertedByMe();
+    client.select(
+      '$obj is a $t at ($ox, $oy)',
+      'mouse is at ($mx, $my)'
+    ).do(({obj, ox, oy, mx, my}) => {
+      const d = dist(ox, oy, mx, my)
       if (d < 100) {
-        assert([$obj, 'is near a mouse'])
-      } else {
-        retract([$obj, 'is near a mouse'])
+        client.assert('_ is near a mouse', obj)
       }
     })
-  },
-  true
-)
-
-orchestrator.addProcess(
-  'learning to not be afraid',
-  'blue',
-  () => {
-    db.query([
-        ['$obj', 'is afraid of mice'],
-        ['$obj', 'can see', '$obj2'],
-        ['$obj2', 'is not afraid of mice'],
-        ['$obj2', 'is near a mouse']
-    ],
-      ({$obj}, facts, assert, retract) => {
-        retract([$obj, 'is afraid of mice'])
-        assert([$obj, 'is not afraid of mice'])
-      }
-    )
   },
   true)
 
 orchestrator.addProcess(
   'fear of mice',
   'red',
-  () => {
-    db.query([
-        ['$obj', 'is near a mouse'],
-        ['$obj', 'is a', '$t', 'at', '(', '$x', ',', '$y', ')'],
-        ['$obj', 'is afraid of mice'],
-        ['mouse', 'is', 'at', '(', '$mx', ',', '$my', ')']
-    ],
-      ({$mx, $my, $obj, $x, $y}, facts, assert, retract) => {
-        retract(facts[1])
-        const newFact = facts[1].slice()
-        const v = 2
-        const dx = v * ($mx > $x ? -1 : 1)
-        const dy = v * ($my > $y ? -1 : 1)
-        newFact[5] += dx
-        newFact[7] += dy
-        assert(newFact)
-      })
+  client => {
+    client.select(
+      '$obj is near a mouse',
+      '$obj is a $t at ($ox, $oy)',
+      '$obj is afraid of mice',
+      '$mouse is at ($mx, $my)'
+    ).do(({obj, t, ox, oy, mx, my}) => {
+      const v = 2
+      const dx = v * (mx > ox ? -1 : 1)
+      const dy = v * (my > oy ? -1 : 1)
+      client.retract('_ is a _ at (_, _)', obj, t, ox, oy)
+      client.assert('_ is a _ at (_, _)', obj, t, ox + dx, oy + dy)
+    })
   },
   true)
+
+orchestrator.addProcess(
+  'learning to not be afraid',
+  'blue',
+  client => {
+    client.select(
+      '$obj1 is afraid of mice',
+      '$obj1 can see $obj2',
+      '$obj2 is not afraid of mice',
+      '$obj2 is near a mouse'
+    ).do(({obj1}) => {
+      client.retract('_ is afraid of mice', obj1)
+      client.assert('_ is not afraid of mice', obj1)
+    })
+  },
+  true)
+
+/*
+
+
 
 let webcamInitStarted = false
 let webcamTrack = null
@@ -296,31 +298,13 @@ orchestrator.addProcess(
   },
   true)
 
-orchestrator.addProcess(
-  'halos',
-  '#abc',
-  () => {
-    db.query(
-      [['$thing', 'is a', 'circle', 'at', '(', '$x', ',', '$y', ')'],
-       ['$thing', '\'s', 'radius', 'is', '$r']],
-      ({$x, $y, $r}, facts, assert, retract) => {
-        illuminate(ctxt => {
-          ctxt.beginPath()
-          ctxt.strokeStyle = 'yellow'
-          ctxt.lineWidth = 10
-          ctxt.arc($x, $y, $r + 20, 0, Math.PI * 2)
-          ctxt.stroke()
-          ctxt.linewidth = 1
-        })
-      })
-  },
-  true)
+*/
 
 let targetObjId = null
 
 let nextIdNum = 1
 function makeNewId () {
-  return 'obj#' + nextIdNum++
+  return new Id('obj' + nextIdNum++)
 }
 
 const colors = ['cornflowerblue', 'steelblue', 'maroon', 'yellow', '#ccc']
@@ -331,9 +315,8 @@ function randomColor () {
 let mouseButtonFact = null
 
 document.body.addEventListener('mousedown', e => {
-  db.retract(mouseButtonFact)
-  mouseButtonFact = ['mouse', 'button', 'is', 'down']
-  db.assert(mouseButtonFact)
+  client.retract('mouse button is $mouseButtonState')
+  client.assert('mouse button is down')
 
   for (let process of orchestrator.processes) {
     if (process.containsPoint(mouse.x, mouse.y)) {
@@ -343,55 +326,43 @@ document.body.addEventListener('mousedown', e => {
 
   if (e.altKey) {
     const obj = makeNewId()
-    const facts = [
-      [obj, 'is a', 'circle', 'at', '(', mouse.x, ',', mouse.y, ')'],
-      [obj, '\'s', 'radius', 'is', 50 + (Math.random() - 0.5) * 20],
-      [obj, '\'s', 'color', 'is', randomColor()]
-    ]
+    client.assert('_ is a circle at (_, _)', obj, mouse.x, mouse.y)
+    client.assert('_\'s radius is _', obj, 50 + (Math.random() - 0.5) * 20)
+    client.assert('_\'s color is _', obj, randomColor())
     if (Math.random() < 0.5) {
-      facts.push([obj, 'is afraid of mice'])
+      client.assert('_ is afraid of mice', obj)
     } else {
-      facts.push([obj, 'is not afraid of mice'])
+      client.assert('_ is not afraid of mice', obj)
     }
-    db.assert(...facts)
   } else if (e.metaKey) {
     const obj = makeNewId()
-    const facts = [
-      [obj, 'is a', 'rectangle', 'at', '(', mouse.x, ',', mouse.y, ')'],
-      [obj, '\'s', 'width', 'is', 50 + (Math.random() - 0.5) * 20],
-      [obj, '\'s', 'height', 'is', 50 + (Math.random() - 0.5) * 20],
-      [obj, '\'s', 'color', 'is', randomColor()]
-    ]
+    client.assert('_ is a rectangle at (_, _)', obj, mouse.x, mouse.y)
+    client.assert('_\'s width is _', obj, 50 + (Math.random() - 0.5) * 20)
+    client.assert('_\'s height is _', obj, 50 + (Math.random() - 0.5) * 20)
+    client.assert('_\'s color is _', obj, randomColor())
     if (Math.random() < 0.5) {
-      facts.push([obj, 'is afraid of mice'])
+      client.assert('_ is afraid of mice', obj)
     } else {
-      facts.push([obj, 'is not afraid of mice'])
+      client.assert('_ is not afraid of mice', obj)
     }
-    db.assert(...facts)
   } else {
-    targetObjId = mouse.targetObj && mouse.targetObj.fact && mouse.targetObj.fact[0]
+    targetObjId = mouse.targetObj && mouse.targetObj.id
   }
   console.log('-----')
   console.log(db.toString())
 })
 
-let mousePosFact = null
-
 document.body.addEventListener('mousemove', e => {
-  db.retract(mousePosFact)
-  mousePosFact = ['mouse', 'is', 'at', '(', mouse.x, ',', mouse.y, ')']
-  db.assert(mousePosFact)
+  client.retract('mouse is at ($x, $y)')
+  client.assert('mouse is at (_, _)', mouse.x, mouse.y)
 
   if (targetObjId) {
-    db.query(
-      [[targetObjId, 'is a', '$t', 'at', '(', '$x', ',', '$y', ')']],
-      ({targetObjId, $t, $x, $y}, facts, assert, retract) => {
-        retract(facts[0])
-        const newFact = facts[0].slice()
-        newFact[5] = mouse.x
-        newFact[7] = mouse.y
-        assert(newFact).withoutEvidence()
-      })
+    client.select(
+      ['_ is a $t at ($x, $y)', targetObjId]
+    ).do(({t, x, y}) => {
+      client.retract('_ is a _ at (_, _)', targetObjId, t, x, y)
+      client.assert('_ is a _ at (_, _)', targetObjId, t, mouse.x, mouse.y)
+    })
   }
 
   console.log('-----')
@@ -399,25 +370,25 @@ document.body.addEventListener('mousemove', e => {
 })
 
 document.body.addEventListener('mouseup', e => {
-  db.retract(mouseButtonFact)
-  mouseButtonFact = ['mouse', 'button', 'is', 'up']
-  db.assert(mouseButtonFact)
+  client.retract('mouse button is $mouseButtonState')
+  client.assert('mouse button is up')
   targetObjId = null
   console.log('-----')
   console.log(db.toString())
 })
 
-const host = window.document.location.host.replace(/:.*/, '')
-let ws = new WebSocket('ws://' + host + ':3000')
 
-ws.onconnect = (event, somethingelse) => {
-  ws.send('it me')
-}
+//const host = window.document.location.host.replace(/:.*/, '')
+//let ws = new WebSocket('ws://' + host + ':3000')
 
-ws.onmessage = event => {
-  console.log(event)
+//ws.onconnect = (event, somethingelse) => {
+//  ws.send('it me')
+//}
+
+//ws.onmessage = event => {
+//  console.log(event)
 //  const assertion = JSON.parse(event.data)
 //  if (typeof assertion == 'Array') {
  //   db.assert(assertion)
   // }
-}
+//}
